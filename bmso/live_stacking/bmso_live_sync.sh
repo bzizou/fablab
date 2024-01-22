843f26db64d6c77d58d3f8083deec1036582a61a
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2024-01-22 17:46:24 +0100

    Live stacking script more verbose

--- bmso/live_stacking/bmso_live_sync.sh
+++ bmso/live_stacking/bmso_live_sync.sh
@@ -4,11 +4,15 @@ TELESCOPE_HOST=buzz
 WATCH_DIR=/home/bzizou/Pictures
 INCOMING_DIR=/home/bzizou/BMSO/live/incoming
 
+echo "Watching $TELESCOPE_HOST:$WATCH_DIR"
+echo "Incoming directory: $INCOMING_DIR"
+
 while [ 1 ]
 do
   file=`ssh $TELESCOPE_HOST inotifywait -q -e create -r $WATCH_DIR --format "%w%f"`
   if [ "$file" \!= "" ]
   then
+	  echo "Getting file $TELESCOPE_HOST:$file ..."
 	  scp $TELESCOPE_HOST:$file $INCOMING_DIR
   fi
   sleep 2
