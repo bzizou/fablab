f162e1c60daa87131c69f6a04ab186c5eeda8eb0
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2025-02-19 14:44:29 +0100

    Fixed nix cache update problem

--- bmso/allsky_cam/bmso_allsky.sh
+++ bmso/allsky_cam/bmso_allsky.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-#nix-shell /home/bzizou/git/fablab/bmso/allsky_cam/default.nix --run "/home/bzizou/git/fablab/bmso/allsky_cam/bmso_allsky.py -S /home/bzizou/Pictures/BMSO_all_sky -s 60 -G -10 -u http://192.168.1.205:81/stream -t 'http://192.168.1.205/cm?cmnd=Status%208'"
-nix-shell /home/bzizou/git/fablab/bmso/allsky_cam/default.nix --run "/home/bzizou/git/fablab/bmso/allsky_cam/bmso_allsky.py -S /home/bzizou/Pictures/BMSO_all_sky -s 10 -G -10 -u http://192.168.1.205:80"
+export NIX_PATH=nixpkgs=/home/bzizou/.nix-defexpr/channels_root/nixos
+#nix-shell /home/bzizou/git/fablab/bmso/allsky_cam/default.nix --run "/home/bzizou/git/fablab/bmso/allsky_cam/bmso_allsky.py -S /home/bzizou/Pictures/BMSO_all_sky -s 10 -G -1 -R -20 -u http://192.168.1.205:80"
+nix-shell /home/bzizou/git/fablab/bmso/allsky_cam/default.nix --run "/home/bzizou/git/fablab/bmso/allsky_cam/bmso_allsky.py -S /home/bzizou/Pictures/BMSO_all_sky -s 6 -G -1 -R -20 -u http://192.168.1.205:80"
